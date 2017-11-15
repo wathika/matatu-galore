079683eb543e85fee95f4d84f099013c9422d11f
wathika <vcwathika@gmail.com>
2017-11-15 08:22:37 +0300

    linked to profiles show page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
     resources :comments
   end
 
+  get ':username', to: 'profile#show', as: 'profile'
+
   root to: "posts#index"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

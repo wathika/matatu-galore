9799ad38ada1a707931ec44df6542e30a014d861
wathika <vcwathika@gmail.com>
2018-02-12 17:02:52 +0300

    fully functioning profile page

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -10,7 +10,19 @@ class ProfilesController < ApplicationController
   end
 
   def update
-
+    @user = User.find_by(username: params[:username])
+    if @user.update(profile_params)
+      flash[:success] = "Your profile has been updated."
+      redirect_to profile_path(@user.username)
+    else
+      @user.errors.full_messages
+      flash[:error] = @user.errors.full_messages
+      render :edit
+    end
   end
 
+protected
+  def profile_params
+    params.require(:user).permit(:avatar, :bio)
+  end
 end

df18de2c9ace538edc665ce95e120e93f705de55
wathika <vcwathika@gmail.com>
2018-02-13 13:38:33 +0300

    displays all who've liked a post

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -7,4 +7,24 @@ module PostsHelper
     image_tag '', id: 'image-preview', class: 'img-responsive'
   end
 
+  def likers_of(post)
+    votes = post.votes_for.up#.by_type(User)
+    user_names = []
+    unless votes.blank?
+      votes.voters.each do |voter|
+        user_names.push(link_to voter.username,
+                                profile_path(voter.username),
+                                class: 'user-name')
+      end
+      user_names.to_sentence.html_safe + like_plural(votes)
+    end
+  end
+
+  private
+
+  def like_plural(votes)
+    return ' like this' if votes.count > 1
+    ' likes this'
+  end
+
 end

3eb4c1d6bd3f2debad5e5d05903d5863e1962ad8
wathika <vcwathika@gmail.com>
2018-02-17 19:25:14 +0300

    can view all notifications

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -4,4 +4,8 @@ class NotificationsController < ApplicationController
     @notification.update read: true
     redirect_to post_path @notification.post
   end
+
+  def index
+    @notifications = current_user.notifications
+  end
 end

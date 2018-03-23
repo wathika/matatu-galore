71be5ad481179a7c46caee1119a396a415dbdaa6
wathika <vcwathika@gmail.com>
2018-03-23 17:43:27 +0300

    fixed filename

--- spec/features/creating_posts_spec.rb
+++ spec/features/creating_posts_spec.rb
@@ -4,7 +4,7 @@ feature 'Creating posts' do
   scenario 'can create a post' do
     visit '/'
     click_link 'New Post'
-    attach_file('Image', "spec/files/images/coffee.jpg")
+    attach_file('Image', "spec/files/images/pic-2.jpg")
     fill_in 'Caption', with: 'nom nom nom #coffeetime'
     click_button 'Create Post'
     expect(page).to have_content('#coffeetime')

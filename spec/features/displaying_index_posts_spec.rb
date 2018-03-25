4b75287ea7e1a1fc4858714c217093e1f68c4fe0
wathika <vcwathika@gmail.com>
2018-03-25 10:20:26 +0300

    view all in index page

---
+++ spec/features/displaying_index_posts_spec.rb
@@ -0,0 +1,17 @@
+# Create multiple posts using factories.
+# User visits the root route.
+# User can see the comments and images of the posts we’ve created.
+
+require 'rails_helper.rb'
+
+feature 'Index displays a list of posts' do
+  scenario 'the index displays correct created post information' do
+    post_one = create(:post, caption: "This is post one")
+    post_two = create(:post, caption: "This is the second post")
+
+    visit '/'
+    expect(page).to have_content('This is post one')
+    expect(page).to have_content('This is the second post')
+    expect(page).to have_css("img[src*='pic-2']")
+  end
+end
